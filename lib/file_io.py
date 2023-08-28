4a167194995a7fc5b917756c913b498eea2cff0e
edmond <edmond.wanyama@student.moringaschool.com>
2023-08-28 23:48:57 +0300

    completed assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,18 @@
 def write_file(file_name, file_content):
-    pass
+    file_path = str(file_name) +'.txt'
+    with open(file_path,mode='w',) as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    file_path = str(file_name) +'.txt'
+    with open(file_path,mode='a',) as file:
+        file.write(append_content)
+    
 
 def read_file(file_name):
-    pass
+    file_path = str(file_name) +'.txt'
+    with open(file_path,mode = 'r') as file:
+        file_content=file.read()
+        return file_content
+
+    
